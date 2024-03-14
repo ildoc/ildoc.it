d4e8c7ec60aabd8caad3c4b959c5951cdb3a1fc0
ildoc <il_doc@protonmail.com>
2024-03-14 22:26:00 +0100

    update dominio

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://ildoc.it'
+SITEURL = 'https://ildoc.dev'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
